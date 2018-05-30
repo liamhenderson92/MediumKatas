bca0da383b5d089014e1105711201a8793d50ea8
Liam Henderson <liam.henderson@digital.hmrc.gov.uk>
2018-05-30 13:14:37 +0100

    changed tests with new 'when' format

--- src/test/scala/NthLargestSpec.scala
+++ src/test/scala/NthLargestSpec.scala
@@ -4,31 +4,36 @@ class NthLargestSpec extends WordSpec with MustMatchers {
 
   "NthLargest" must {
 
-    "return the smallest number when given 2 different numbers and n equal to 2" in {
-      NthLargest.calculation(List(1,2),2) mustEqual 1
+    "return the correct elements" when {
+
+      "given 2 different numbers and n equal to 2" in {
+        NthLargest.calculation(List(1, 2), 2) mustEqual 1
+      }
+
+      "given 2 different numbers and n equal to 1" in {
+        NthLargest.calculation(List(1, 2), 1) mustEqual 2
+      }
+
+      "given a small list and n equal to 2" in {
+        NthLargest.calculation(List(1, 2, 3, 4, 5), 2) mustEqual 4
+      }
+
+      "given a largest list and n equal to 4" in {
+        NthLargest.calculation(List(1, 4, 6, 7, 2, 3, 1, 5, 4, 5), 4) mustEqual 4
+      }
+
+      "given a larger distinct list and n equal to 3" in {
+        NthLargest.calculation(List(3, 3, 4, 4, 5, 5), 3) mustEqual 3
+      }
     }
 
-    "return the largest number when given 2 different numbers and n equal to 1" in {
-      NthLargest.calculation(List(1,2),1) mustEqual 2
-    }
+    "throw an Illegal Argument Exception" when {
 
-    "return the second largest number when given a small list and n equal to 2" in {
-      NthLargest.calculation(List(1,2,3,4,5),2) mustEqual 4
+      "when given a list of 2 numbers and n equal to 3" in
+        intercept[IllegalArgumentException] {
+          NthLargest.calculation(List(1, 2), 3)
+        }
     }
-
-    "return the fourth largest number when given a largest list and n equal to 4" in {
-      NthLargest.calculation(List(1,4,6,7,2,3,1,5,4,5),4) mustEqual 4
-    }
-
-    "return the third largest number when given a larger distinct list and n equal to 3" in {
-      NthLargest.calculation(List(3,3,4,4,5,5),3) mustEqual 3
-    }
-
-    "throw an exception when given a list of 2 numbers and n equal to 3" in
-      intercept[IllegalArgumentException]{
-      NthLargest.calculation(List(1,2),3)
-    }
-
   }
 
 }
